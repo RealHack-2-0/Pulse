ae1a2664beaa5f3bbdcd32d211db181aa1393003
Anju Chamantha <chamantha97anju@gmail.com>
2019-11-07 23:02:54 +0530

    designed answer widget

--- qna-flutter/lib/views/question/answer.dart
+++ qna-flutter/lib/views/question/answer.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Answer"),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Text("A"),
+            ),
+            title: Text("Anju Chamantha"),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(75, 0, 20, 5),
+          child: Text(
+              "My answer for this question is unknown. My answer for this question is unknown. My answer for this question is unknown. My answer for this question is unknown. My answer for this question is unknown. My answer for this question is unknown"),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.check),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(20),
+              ),
+              color: Colors.grey[200],
+              label: Text("Mark as correct"),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        Divider(),
+      ],
     );
   }
 }
--- qna-flutter/lib/views/question/question_screen.dart
+++ qna-flutter/lib/views/question/question_screen.dart
@@ -26,6 +26,7 @@ class QuestionScreen extends StatelessWidget {
           ),
           Divider(),
         Answer(),
+        Answer(),
         ],
       ),
     );
