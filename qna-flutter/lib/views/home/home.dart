cca05347454d64a30e9eeb4cc07d39b5d7c7cdf1
Anju Chamantha <chamantha97anju@gmail.com>
2019-11-08 00:53:30 +0530

    Update home.dart

--- qna-flutter/lib/views/home/home.dart
+++ qna-flutter/lib/views/home/home.dart
@@ -7,20 +7,31 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // bottomNavigationBar: BottomNavigationBar(),
-      appBar: AppBar(title: Text("Stack Overflow Demo"),
-      actions: <Widget>[
-
-      ],),
+      appBar: AppBar(
+        title: Text("Stack Overflow Demo"),
+        actions: <Widget>[],
+      ),
       drawer: MyDrawer(),
       body: Container(
-        child:ListView(
-          children: <Widget>[
-            QuestionView(title: "How to add a image to Firebase?", subtitle: "10 minutes ago", upVotes: 15,downVotes:3),
-            QuestionView(title: "How to center a widget in Flutter?", subtitle: "5 seconds ago", upVotes: 9,downVotes:63),
-            QuestionView(title: "Can it possible to have good life as a programmer?", subtitle: "10 hours ago", upVotes: 9,downVotes:3)
-          ],
-        )
-      ),
-    );  
+          child: ListView(
+        children: <Widget>[
+          QuestionView(
+              title: "How to add a image to Firebase?",
+              subtitle: "10 minutes ago",
+              upVotes: 15,
+              downVotes: 3),
+          QuestionView(
+              title: "How to center a widget in Flutter?",
+              subtitle: "5 seconds ago",
+              upVotes: 9,
+              downVotes: 63),
+          QuestionView(
+              title: "Can it possible to have good life as a programmer?",
+              subtitle: "10 hours ago",
+              upVotes: 9,
+              downVotes: 3)
+        ],
+      )),
+    );
   }
 }
